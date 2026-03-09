795b53b02c9795dc7ad9fd65684b7bd0e5a2246a
Your Name <you@example.com>
2026-03-09 10:59:58 -0300

    Adicionar index2.php

---
+++ gabriel-guilherme/exercicios-php/index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get">
+        <label>Informe a temperatura: </label>
+        <input type="number" name="t" step="any" required>
+        <br><br>
+
+        <label>Celsius(C) ou Fahrenheit</label>
+    </form>
+</body>
+</html>
